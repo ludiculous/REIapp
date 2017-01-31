52b48a4f56b35c252009a33fbd61f238965c73b0
Preston <ludiculous89@gmail.com>
2017-01-31 21:59:42 +0700

    second

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost/REItraffic'
+  database: 'mongodb://pchen415:123456@ds137149.mlab.com:37149/reitraffic'
 };
-//process.env.MONGO_URL = 'mongodb://pchen415:123456@ds137149.mlab.com:37149/reitraffic'
+//||'mongodb://localhost/REItraffic'
