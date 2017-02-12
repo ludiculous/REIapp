471fb7f21280cef9aae0e27992fea6f721b9bca2
Preston <ludiculous89@gmail.com>
2017-02-12 21:41:11 +0700

    ZHS

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import { gridReducers} from 'react-redux-grid';
-//import { Reducers as gridReducers } from 'react-redux-grid';
 import authReducer from './AuthReducer';
 import marketReducer from './MarketReducer';
 
